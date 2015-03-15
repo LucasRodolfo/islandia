5358a543572a9b3d3ab51723fe185c4a9c556346
fakegermano <danielgtravieso@gmail.com>
2015-03-15 06:04:16 -0300

    home for guest users is ready

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -11,39 +11,39 @@
         <div class="col-md-12 column">
             <nav class="navbar navbar-default" role="navigation">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <button type="button" class="btn btn-primary btn-lg">Default</button>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                 </div>
-
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li><a class="btn btn-primary fix-button" href="#"><div class="style-login">Ir para estatuto</div></a></li>
+                    </ul>
                     <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#">Link</a>
-                        </li>
-                        <li>
-                            <img class="img-circle" alt="140x140" src="http://lorempixel.com/140/140/" />
-                        </li>
+                        <li><a class="btn btn-default fix-button" href="#"><strong class="style-login">Login</strong></a></li>
                     </ul>
                 </div>
-
             </nav>
         </div>
     </div>
     <div class="row clearfix">
         <div class="col-md-12 column">
-            <img class="img-circle" alt="140x140" src="http://lorempixel.com/140/140/" />
-            <h3>
-                h3. Lorem ipsum dolor sit amet.
-            </h3>
-            <p>
-                Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
-            </p>
+            <div class="center">
+                {{ HTML::image('bootstrap/img/logoUnicamp.png', '', array('class'=>'img-circle big-landing-image', 'alt'=>'logo unicamp')) }}
+                <h3>
+                    Project Iceland
+                </h3>
+                <p>
+                    Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+                </p>
+            </div>
         </div>
     </div>
 </div>
 @else
 
-<!-- OR IF LOGGED IN -->
 <div class="container">
     <div class="row clearfix">
         <div class="col-md-12 column">
