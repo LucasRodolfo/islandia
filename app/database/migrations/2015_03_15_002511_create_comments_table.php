5c8fef5b2e675148f21c1b811523634c28903f29
fbidu <bidu.pub@gmail.com>
2015-03-14 21:27:27 -0300

    Comments migration

---
+++ app/database/migrations/2015_03_15_002511_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateCommentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('comments', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->text('content');
+            $table->integer('user')->unsigned();
+            $table->foreign('user')->references('id')->on('users');
+            $table->integer('reply')->unsigned();
+            $table->foreign('reply')->references('id')->on('replies');
+			$table->timestamps();
+            $table->softDeletes();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('comments');
+	}
+
+}
