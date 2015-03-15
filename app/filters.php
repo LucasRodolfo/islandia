38080bc0a2e10be60c61357aa7027a30553726b0
fakegermano <danielgtravieso@gmail.com>
2015-03-15 19:53:36 -0300

    admin views and Controller init

--- app/filters.php
+++ app/filters.php
@@ -88,3 +88,16 @@ Route::filter('csrf', function()
 		throw new Illuminate\Session\TokenMismatchException;
 	}
 });
+
+Route::filter('admin', function()
+{
+    if (Auth::guest()==false)
+    {
+        if (Auth::user()->category() == 'Administradores') {
+            return Redirect::to('admin');
+        }
+        else {
+            return Redirect::to('logout');
+        }
+    }
+});
